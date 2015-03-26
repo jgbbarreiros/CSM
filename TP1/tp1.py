1c5ca862e1dcb4e4e1be44b75b5805db4ecaa4e5
Joao Barreiros <jgbbarreiros@gmail.com>
2015-03-26 00:09:33 +0000

    Exercicio 7 terminado, falta limpar e compactar

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -4,96 +4,93 @@ import matplotlib.pyplot as plt
 
 # ex 1
 
-plt.figure("ex 1")
+# remove comment plt.figure("ex 1")
 x_img = Image.open("lenac.tif")
-plt.imshow(x_img, interpolation='nearest')
+# remove comment plt.imshow(x_img, interpolation='nearest')
 print x_img.format
 print x_img.mode
 print x_img.size
 
 # ex 2
 
-x_img.save("file1.jpg", "JPEG", quality=80)
-x_img.save("file2.jpg", "JPEG", quality=10)
+# remove comment x_img.save("file1.jpg", "JPEG", quality=80)
+# remove comment x_img.save("file2.jpg", "JPEG", quality=10)
 
 # ex 3
 
-plt.figure("ex 3")
+# remove comment plt.figure("ex 3")
 x_gray = x_img.convert("L")
-plt.imshow(x_gray, interpolation='nearest', cmap='gray')
-x_gray.save("file2.bmp", "bmp")
+# remove comment plt.imshow(x_gray, interpolation='nearest', cmap='gray')
+# remove comment x_gray.save("file2.bmp", "bmp")
 
 # ex 4
 
-plt.figure("ex 4")
+# remove comment plt.figure("ex 4")
 hist = x_img.histogram()
-plt.plot(hist)
+# remove comment plt.plot(hist)
 
 # ex 5
 
-plt.figure("ex 5")
+# remove comment plt.figure("ex 5")
 x = np.array(x_gray)
 
 for i in range(8):
     y = (x >= 2**i) & (x < 2**(i+1))
-    plt.subplot(3,3,8-i)
-    plt.imshow(y, cmap='gray')
+    # remove comment plt.subplot(3,3,8-i)
+    # remove comment plt.imshow(y, cmap='gray')
 
 
 # ex 6
 
-plt.figure("ex 6")
+# remove comment plt.figure("ex 6")
 y = x >= 2**5
-plt.imshow(y, cmap='gray')
-new_img = Image.fromarray(y.astype('uint8')*255 ,'L')
-new_img.save("lena_4.bmp")
+# remove comment plt.imshow(y, cmap='gray')
+# remove comment new_img = Image.fromarray(y.astype('uint8')*255 ,'L')
+# remove comment new_img.save("lena_4.bmp")
 
 
 # ex 7
 
-# plt.figure("ex 7")
-# lsd_w = 100
-# lsd_h = 100
-
-# lsd = np.zeros((lsd_h, lsd_w))
-# cursor = [lsd_h/2, lsd_w/2]
-
-# r1 = np.zeros((lsd_w/2, 2))
-# r2 = np.zeros((lsd_w/2, 2))
-
-# for i in range(lsd_w/2):
-#     r1[i] = [i + lsd_w/2, lsd_h/2]
-#     r2[i] = [i + lsd_w/2, i*np.tan(2*np.pi/18.)]
-
-# print r1
-# print r2
-
-# for x in range(lsd_w):
-#     for y in range(lsd_h):
-#         if (x > r2[x][0])
-#         if (x > r2[y][0]+lsd_w/2 & y > r2[y][])
-#             if (x > r2[x-lsd_w/2])
-
 plt.figure("ex 7")
 
-w = 100
-h = 100
+w = 1000
+h = 1000
 
 star_burst = np.ones((h, w))*255
 
+angle = 20
+
+rad = angle * np.pi / 180.
+
 c = [w/2, h/2]
 
-for x in range(w):
-    for y in range(h):
+for y in range(h):
+    for x in range(w):
         v = [x - c[0], c[1] - y]
+        #print "[" + str(v[0]) + ", " + str(v[1]) + "]"
         try:
-            a = np.arctan(float(v[0])/v[1])
+            if ((v[0] < 0) & (v[1] > 0)):
+                a = np.arctan(float(v[0])/v[1])
+            elif ((v[0] > 0) & (v[1] < 0)):
+                a = np.arctan(float(v[0])/v[1])
+            else:
+                a = np.arctan(float(v[1])/v[0])
         except Exception, e:
             a = 0
-        for i in range(5):
-            if (i%2 == 0):
-                if ( (a >= (2*np.pi/18.*i)) & (a < (2*np.pi/18.*(i+1)) ) ):
-                    star_burst[x, y] = 0
+        #print a
+        if ((v[0] < 0) & (v[1] > 0)):
+            a += np.pi/2.
+        if ((v[0] < 0) & (v[1] < 0)):
+            a += np.pi
+        if ((v[0] > 0) & (v[1] < 0)):
+            a += 3*np.pi/2.
+        #print a
+        #print "\n"
+        for i in range(180/angle):
+            if (rad*i*2 <= a < rad*(i*2+1)):
+                star_burst[y, x] = 0
+
+#star_burst[99, 80] = 0
 
 plt.imshow(star_burst, cmap='gray')
 
