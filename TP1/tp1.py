3d349ecd64c53f1935136e3cac78f6c7f317d740
Joao Barreiros <jgbbarreiros@gmail.com>
2015-03-27 01:11:07 +0000

    Adicionado ficheiro de texto com os intervalos dos angulos do ex7 tp1. Versão final do ex 7 desta vez já funciona com outros angulos. Necessita limpeza e compactação.

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -53,77 +53,43 @@ y = x >= 2**5
 
 plt.figure("ex 7")
 
-w = 1000
-h = 1000
+
+w = 400
+h = 400
 
 star_burst = np.ones((h, w))*255
-
-angle = 20
+angle = 16
 
 rad = angle * np.pi / 180.
-
-c = [w/2, h/2]
-
-# for y in range(h):
-#     for x in range(w):
-#         v = [x - c[0], c[1] - y]
-#         #print "[" + str(v[0]) + ", " + str(v[1]) + "]"
-#         try:
-#             if ((v[0] < 0) & (v[1] > 0)):
-#                 a = np.arctan(float(v[0])/v[1])
-#             elif ((v[0] > 0) & (v[1] < 0)):
-#                 a = np.arctan(float(v[0])/v[1])
-#             else:
-#                 a = np.arctan(float(v[1])/v[0])
-#         except Exception, e:
-#             a = 0
-#         #print a
-#         if ((v[0] < 0) & (v[1] > 0)):
-#             a += np.pi/2.
-#         if ((v[0] < 0) & (v[1] < 0)):
-#             a += np.pi
-#         if ((v[0] > 0) & (v[1] < 0)):
-#             a += 3*np.pi/2.
-#         #print a
-#         #print "\n"
-#         for i in range(180/angle):
-#             if (rad*i*2 <= a < rad*(i*2+1)):
-#                 star_burst[y][x] = 0
-
-n = np.pi/2. / rad
-#n = 90. / angle
-fraq = (n - (round(n)-1))
-print fraq
-n = 3*np.pi/2. / rad
-fraq2 = (n - (round(n)-1))
-print fraq2
-
 
 for y in range(h):
     for x in range(w):
         vx = x - float(w/2)
         vy = float(h/2) - y
+
         if vx == 0:
-            a = 0
-        if vx*vy <= 0:
-            if vy == 0:
-                a = np.pi/2.
-            else:
-                a = np.arctan(vx/vy) + np.pi/2. - (fraq*rad)
-            if vx > 0:
-                a += np.pi + (fraq - fraq2)
+            a = .1
         else:
             a = np.arctan(vy/vx)
-            if vx < 0:
-                a += np.pi
-            
-        for i in range(180/angle):
+        # 1 quadrante
+        if (vx > 0) & (vy > 0):
+            pass
+
+        # 2 quadrante
+        if (vx < 0) & (vy > 0):
+            a += np.pi
+
+        # 3 quadrante
+        if (vx < 0) & (vy < 0):
+            a += np.pi
+
+        # 4 quadrante
+        if (vx > 0) & (vy < 0):
+            a += 2*np.pi
+
+        for i in range(180/angle + 1):
             if (rad*i*2 <= a < rad*(i*2+1)):
                 star_burst[y][x] = 0
-
-
-
-#star_burst[99, 80] = 0
 
 plt.imshow(star_burst, cmap='gray')
 
