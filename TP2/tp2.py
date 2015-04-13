d3a453a578392dc20a37e0078e054955745ec55f
Joao Barreiros <jgbbarreiros@gmail.com>
2015-04-13 18:16:50 +0100

    Ficheiro tp2 reformatado

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -3,50 +3,51 @@ import numpy as np
 
 # functions
 
-def bin_code_shannon_fano(symb, prob):
-  i = np.argsort(prob)[::-1][:len(prob)]
-  s = sorted(prob, reverse=True)
-  c = code_tree(s)
-  f = np.empty((len(c)), dtype='|S' + str(len(c)))
 
-  for x in range(len(c)):
-    f[i[x]] = c[x]
+def bin_code_shannon_fano(prob):
+    i = np.argsort(prob)[::-1][:len(prob)]
+    s = sorted(prob, reverse=True)
+    c = code_tree(s)
+    f = np.empty((len(c)), dtype='|S' + str(len(c)))
 
-  return f
+    for x in range(len(c)):
+        f[i[x]] = c[x]
+
+    return f
 
 
-  # [.20, .20, .20, .10, .10, .10, .5, .5]
 def code_tree(prob):
-  for i in range(len(prob)):
-    l = prob[:i+1]
-    r = prob[i+1:]
-    if (sum(l) >= sum(r)):
-      break
-  if len(l) == 1:
-    fl = np.array(["0"], dtype='|S100')
-  else:
-    fl = code_tree(l)
-    for i in range(len(fl)):
-      fl[i] = "0" + fl[i]
+    l = 0
+    r = 0
+    for i in range(len(prob)):
+        l = prob[:i + 1]
+        r = prob[i + 1:]
+        if sum(l) >= sum(r):
+            break
+    if len(l) == 1:
+        fl = np.array(['0'], dtype='|S100')
+    else:
+        fl = code_tree(l)
+        for i in range(len(fl)):
+            fl[i] = '0' + str(fl[i])
 
-  if len(r) == 1:
-    fr = np.array(["1"], dtype='|S100')
-  else:
-    fr = code_tree(r)
-    for i in range(len(fr)):
-      fr[i] = "1" + fr[i]
-  return np.hstack((fl, fr))
+    if len(r) == 1:
+        fr = np.array(['1'], dtype='|S100')
+    else:
+        fr = code_tree(r)
+        for i in range(len(fr)):
+            fr[i] = '1' + str(fr[i])
+    return np.hstack((fl, fr))
 
 
 # main
 
 if __name__ == "__main__":
 
-  # ex 1
-  probability = [.10,.20,.30,.10,.30]
+    # ex 1
+    probability = [.10, .20, .30, .10, .30]
 
-  bin_table = bin_code_shannon_fano(symbols, probability)
+    bin_table = bin_code_shannon_fano(probability)
+    print bin_table
 
-  # ex 2
-
-  
+    # ex 2
