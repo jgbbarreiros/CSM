70d1585cd519d05d053a5e5c5c49a217ef1fcbbf
Joao Barreiros <jgbbarreiros@gmail.com>
2015-04-19 19:12:39 +0100

    limpeza de codigo

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -99,12 +99,9 @@ if __name__ == "__main__":
     lena = Image.open("lenac.tif").convert("L")
     hist = lena.histogram()
     before = millis()
-    print 'qualquer'
     test_lena_code = bin_code_shannon_fano(zeros(hist)[0])
     diff = millis() - before
     print diff
-
-    print "test"
 
 
     #B
