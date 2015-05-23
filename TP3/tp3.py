7bf380fb6ef1b51077f2d6d26404963a8f3ea0f4
joaoolival <joaoljolival@gmail.com>
2015-05-23 18:27:51 +0100

    junta o blockshaped e o blockshaped é para substituir pelo getBlocos e por um suposto "un"getBlocos

--- TP3/tp3.py
+++ TP3/tp3.py
@@ -17,6 +17,12 @@ def getBlocos(img):
             Blocos.append(I[x*8:(x+1)*8, y*8:(y+1)*8])
     return Blocos
 
+def blockshaped(arr, nrows, ncols):
+    h, w = arr.shape
+    return (arr.reshape(h//nrows, nrows, -1, ncols)
+               .swapaxes(1,2)
+               .reshape(-1, nrows, ncols))
+
 def getDct(bloc):
     return dct(dct(bloc.T*1., norm='ortho').T , norm='ortho')
 
@@ -69,7 +75,9 @@ if __name__ == "__main__":
     I = readImage('lena.tiff')
     print "Image %s:\n%s\n" % (I.shape, I)
 
-    Blocos = getBlocos(I)
+    Blocos = blockshaped(I,8,8)
+
+    #Blocos = getBlocos(I)
 
 
     Blocos = np.asarray(Blocos)
@@ -85,93 +93,93 @@ if __name__ == "__main__":
     DC = []
     code = []
 
-    # compressao
-    for B in Blocos:
-
-        # dct
-        C = getDct(B)
-        # print "DCT:\n%s\n" % C
-
-        # idct
-        # B2 = idct(idct(C.T*1., norm='ortho').T , norm='ortho')
-        # print "IDCT (comparacao):\n%s\n%s\n" % (B, B2)
-
-        # quantificacao
-        BQ = np.round(C/(a*Q)).astype('int')
-        print "Bloco quantificado:\n%s\n" % BQ
-
-        # desquantificao
-        # C2 = a*Q*BQ
-        # print "Bloco desquantificado (comparacao):\n%s\n%s\n" % (C, C2)
-
-        # codificador DC
-        if len(DC) != 0:
-            DC_diff = BQ[0][0] - DC[-1]
-        else:
-            DC_diff = BQ[0][0]
-        print "DC = %s" % DC_diff
-        DC.append(BQ[0][0])
-        DC_code = ''
-        if DC_diff < 0:
-            DC_bin = bin(DC_diff)[3:]
-            DC_code += K3.get(len(DC_bin)) + twos_complement(DC_bin)
-        elif DC_diff == 0:
-            DC_code += "00"
-        else:
-            DC_bin = bin(DC_diff)[2:]
-            DC_code += K3.get(len(DC_bin)) + DC_bin
-
-        code += map(int, DC_code)
-
-        # codificador AC
-        AC_code = ''
-        numZeros = 0
-        BQ_zz = BQ.flatten(order='F')[np.argsort(ind_zz)].astype(int)
-        #print BQ_zz
-        print "AC = ",
-        # for i in range(1, len(BQ_zz)):
-
-        i = 1
-        while i < len(BQ_zz):
-            if BQ_zz[i] != 0:
-                print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
-                AC_code += K5.get((numZeros, len(bin(abs(BQ_zz[i]))[2:])))
-                # print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
-                # print "---0"
-                if BQ_zz[i] < 0:
-                    AC_bin = bin(BQ_zz[i])[3:]
-                    AC_code += twos_complement(AC_bin)
-                    print twos_complement(AC_bin),
-                    # print "---1"
-                elif BQ_zz[i] > 0:
-                    AC_code += bin(BQ_zz[i])[2:]
-                    print BQ_zz[i],
-                    # print "---2"
-                numZeros = 0
-            else:
-                #se do i para a frente for tudo zero
-                if sum(BQ_zz[i:]!=0) == 0:
-                    AC_code += K5.get((0,0))
-                    print (0,0)
-                    break
-                #se tivermos dentro do len e se nos proximos 15 forem todos zero
-                if (i + 15) < len(BQ_zz) and sum(BQ_zz[i:i+15]!=0) == 0:
-                    AC_code += K5.get((15,len(bin(abs(BQ_zz[i+15]))[2:])))
-                    print (15,len(bin(abs(BQ_zz[i+15]))[2:])),
-                    i += 16
-                    numZeros =0
-                    pass
-                numZeros +=1
-            i += 1
-
-        code += map(int, AC_code)
-
-        print 'DC_code %s' % DC_code
-        print 'AC_code %s' % AC_code
-        print "\n--------------------------"
-
-    print 'code %s' % code
-
-    write(code, 'lena')
-
-    descompressao
+    # # compressao
+    # for B in Blocos:
+    #
+    #     # dct
+    #     C = getDct(B)
+    #     # print "DCT:\n%s\n" % C
+    #
+    #     # idct
+    #     # B2 = idct(idct(C.T*1., norm='ortho').T , norm='ortho')
+    #     # print "IDCT (comparacao):\n%s\n%s\n" % (B, B2)
+    #
+    #     # quantificacao
+    #     BQ = np.round(C/(a*Q)).astype('int')
+    #     print "Bloco quantificado:\n%s\n" % BQ
+    #
+    #     # desquantificao
+    #     # C2 = a*Q*BQ
+    #     # print "Bloco desquantificado (comparacao):\n%s\n%s\n" % (C, C2)
+    #
+    #     # codificador DC
+    #     if len(DC) != 0:
+    #         DC_diff = BQ[0][0] - DC[-1]
+    #     else:
+    #         DC_diff = BQ[0][0]
+    #     print "DC = %s" % DC_diff
+    #     DC.append(BQ[0][0])
+    #     DC_code = ''
+    #     if DC_diff < 0:
+    #         DC_bin = bin(DC_diff)[3:]
+    #         DC_code += K3.get(len(DC_bin)) + twos_complement(DC_bin)
+    #     elif DC_diff == 0:
+    #         DC_code += "00"
+    #     else:
+    #         DC_bin = bin(DC_diff)[2:]
+    #         DC_code += K3.get(len(DC_bin)) + DC_bin
+    #
+    #     code += map(int, DC_code)
+    #
+    #     # codificador AC
+    #     AC_code = ''
+    #     numZeros = 0
+    #     BQ_zz = BQ.flatten(order='F')[np.argsort(ind_zz)].astype(int)
+    #     #print BQ_zz
+    #     print "AC = ",
+    #     # for i in range(1, len(BQ_zz)):
+    #
+    #     i = 1
+    #     while i < len(BQ_zz):
+    #         if BQ_zz[i] != 0:
+    #             print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
+    #             AC_code += K5.get((numZeros, len(bin(abs(BQ_zz[i]))[2:])))
+    #             # print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
+    #             # print "---0"
+    #             if BQ_zz[i] < 0:
+    #                 AC_bin = bin(BQ_zz[i])[3:]
+    #                 AC_code += twos_complement(AC_bin)
+    #                 print twos_complement(AC_bin),
+    #                 # print "---1"
+    #             elif BQ_zz[i] > 0:
+    #                 AC_code += bin(BQ_zz[i])[2:]
+    #                 print BQ_zz[i],
+    #                 # print "---2"
+    #             numZeros = 0
+    #         else:
+    #             #se do i para a frente for tudo zero
+    #             if sum(BQ_zz[i:]!=0) == 0:
+    #                 AC_code += K5.get((0,0))
+    #                 print (0,0)
+    #                 break
+    #             #se tivermos dentro do len e se nos proximos 15 forem todos zero
+    #             if (i + 15) < len(BQ_zz) and sum(BQ_zz[i:i+15]!=0) == 0:
+    #                 AC_code += K5.get((15,len(bin(abs(BQ_zz[i+15]))[2:])))
+    #                 print (15,len(bin(abs(BQ_zz[i+15]))[2:])),
+    #                 i += 16
+    #                 numZeros =0
+    #                 pass
+    #             numZeros +=1
+    #         i += 1
+    #
+    #     code += map(int, AC_code)
+    #
+    #     print 'DC_code %s' % DC_code
+    #     print 'AC_code %s' % AC_code
+    #     print "\n--------------------------"
+    #
+    # print 'code %s' % code
+    #
+    # write(code, 'lena')
+    #
+    # descompressao
