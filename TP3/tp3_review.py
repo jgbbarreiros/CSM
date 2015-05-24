361ab0e27cd6c344c55a4d2b6f1dd91521fd5146
Joao Barreiros <jgbbarreiros@gmail.com>
2015-05-24 22:54:51 +0100

    TP3 finalizações bug fixes limpeza de código graficos, tempos, tamanhos, taxas de compressão

--- TP3/tp3_review.py
+++ TP3/tp3_review.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy as sp
 from time import time
 from os import path
+import matplotlib.pyplot as plt
 import scipy.misc as mi
 from Tables_jpeg import *
 
@@ -13,24 +14,20 @@ def readImage(name):
 
 def blockShaped(arr, nrows, ncols):
     h, w = arr.shape
-    return (arr.reshape(h//nrows, nrows, -1, ncols)
-               .swapaxes(1,2)
-               .reshape(-1, nrows, ncols))
+    return (arr.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols))
 
 def unblockShaped(arr, h, w):
     #array, 512,512
     n, nrows, ncols = arr.shape
-    return (arr.reshape(h//nrows, -1, nrows, ncols)
-               .swapaxes(1,2)
-               .reshape(h, w))
+    return (arr.reshape(h//nrows, -1, nrows, ncols).swapaxes(1,2).reshape(h, w))
 
-def DCT(blocksIMG):
+def DCTransform(blocksIMG):
     DCT = []
     for block in blocksIMG:
         DCT.append(dct(dct(block.T*1., norm='ortho').T , norm='ortho'))
     return DCT
 
-def IDCT(blocksDCT):
+def IDCTransform(blocksDCT):
     IDCT = []
     for block in blocksDCT:
         IDCT.append(idct(idct(block.T*1., norm='ortho').T , norm='ortho').astype('int'))
@@ -89,8 +86,6 @@ def code(blocksQ):
         # print BQ_zz
 
         # print "AC =",
-        # for i in range(1, len(BQ_zz)):
-
         i = 1
         while i < len(BQ_zz):
             if BQ_zz[i] != 0:
@@ -102,8 +97,12 @@ def code(blocksQ):
                     # print twos_complement(AC_bin),
                 elif BQ_zz[i] > 0:
                     AC_code += bin(BQ_zz[i])[2:]
-                    # print BQ_zz[i],
+                    # print bin(BQ_zz[i])[2:],
                 numZeros = 0
+                if i+1 >= len(BQ_zz):
+                    AC_code += K5.get((0,0))
+                    # print (0,0)
+                    break
             else:
                 #se do i para a frente for tudo zero
                 if sum(BQ_zz[i:]!=0) == 0:
@@ -219,58 +218,88 @@ def write(seqBits, fileName):
 def read(fileName):
     compressedFile = np.load(fileName)
     seqBits = np.unpackbits(compressedFile)
-    return seqBits[:-7]
+    return seqBits[:len(CODE)]
 
 
 if __name__ == "__main__":
 
-    quality = 50
-
-    I = readImage('lena.tiff')
-    print "Image %s:\n%s\n" % (I.shape, I)
-
-    BLOCKS = blockShaped(I, 8, 8)
-    # print "Blocks:\n%s\n" % BLOCKS
-
-    a = quality_factor(quality)
-    # print "alfa:\n%s\n" % a
-
-    DCT = DCT(BLOCKS)
-    # print "DCT:\n%s\n" % DCT
-
-    BQ = quantify(DCT)
-    # print "Quantified:\n%s\n" % BQ
-
-    CODE = code(BQ)
-    # print "code:\n%s\n" % CODE
-
-    write(CODE, 'lena')
-    CODE1 = read('lena.npy')
-
-    BQ1 = decode(CODE1)
-    # print "Quantified new:\n%s\n" % BQ1
-
-    DCT1 = dequantify(BQ1)
-    # print "DCT new:\n%s\n" % DCT1
-
-    BLOCKS1 = IDCT(DCT1)
-    # print "BLOCKS new:\n%s\n" % BLOCKS1
-
-    I1 = unblockShaped(np.asarray(BLOCKS1), 512, 512)
-    print "Image new:\n%s\n" % I1
-
-    img = Image.fromarray(I1.astype('uint8'), 'L')
-    img.save("lenaJPEG.bmp")
-
-    sizeIni = path.getsize('lena.tiff')
-    sizeEnd = path.getsize('lena.npy')
+    sizeIni = 1. * path.getsize('lena.tiff')
     print "tamanho lena.tiff %s" % sizeIni
-    print "tamanho lena.npy  %s" % sizeEnd
-    print "taxa: %s" % (1. * sizeIni / sizeEnd)
+    qualitys = [10,25,50,75,90]
+    snrs = []
+    txs  = []
 
-    print ""
-    print "snr"
-    print SNR(I,I1)
-
+    for i in range(len(qualitys)):
 
 
+        quality = qualitys[i]
+        print 'comprimindo imagem com qualidade %s...' % quality
+
+        I = readImage('lena.tiff')
+        # print "Image %s:\n%s\n" % (I.shape, I)
+
+        BLOCKS = blockShaped(I, I.shape[0]/64, I.shape[1]/64)
+        # print "Blocks:\n%s\n" % BLOCKS
+
+        a = quality_factor(quality)
+        # print "alfa:\n%s\n" % a
+
+        DCT = DCTransform(BLOCKS)
+        # print "DCT:\n%s\n" % DCT
+
+        BQ = quantify(DCT)
+        # print "Quantified:\n%s\n" % BQ
+
+        t0 = time()
+        CODE = code(BQ)
+        t1 = time()
+        print "time code: " + str(t1 - t0)
+        # print "code:\n%s\n" % CODE
+
+        write(CODE, 'lena'+str(quality))
+        CODE1 = read('lena'+str(quality)+'.npy')
+        # print "code new:\n%s\n" % CODE1
+
+        t0 = time()
+        BQ1 = decode(CODE1)
+        t1 = time()
+        print "time decode: " + str(t1 - t0)
+        # print "Quantified new:\n%s\n" % BQ1
+
+        DCT1 = dequantify(BQ1)
+        # print "DCT new:\n%s\n" % DCT1
+
+        BLOCKS1 = IDCTransform(DCT1)
+        # print "BLOCKS new:\n%s\n" % BLOCKS1
+
+        I1 = unblockShaped(np.asarray(BLOCKS1), 512, 512)
+        # print "Image new:\n%s\n" % I1
+
+        img = Image.fromarray(I1.astype('uint8'), 'L')
+        img.save("lenaJPEG"+str(quality)+".bmp")
+
+        snr = SNR(I,I1)
+        print "snr: %s" % snr
+        snrs.append(snr)
+
+        sizeEnd = path.getsize('lena'+str(quality)+'.npy')
+        print "lena.npy size:  %s" % sizeEnd
+
+        tx = sizeIni/sizeEnd
+        print "taxa: %s" % tx
+        txs.append(tx)
+        print
+
+
+    plt.figure()
+    plt.grid(True)
+    plt.plot(snrs, txs)
+    plt.xlabel("SNR")
+    plt.ylabel("taxa de compressao")
+
+    x = Image.open("lena.tiff")
+    x.save("lena.jpeg", "JPEG", quality=50)
+    sizeJPG = path.getsize('lena.jpeg')
+    print "taxa jpg: %s" % (sizeIni/sizeJPG)
+
+    plt.show()
