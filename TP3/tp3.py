116e7aa863878c410c524494642825afdc89b6b6
Joao Barreiros <jgbbarreiros@gmail.com>
2015-05-24 23:02:36 +0100

    actualização de ficheiros

--- TP3/tp3.py
+++ TP3/tp3.py
@@ -4,42 +4,46 @@ import numpy as np
 import scipy as sp
 from time import time
 from os import path
+import matplotlib.pyplot as plt
 import scipy.misc as mi
 from Tables_jpeg import *
 
 def readImage(name):
     return mi.imread('lena.tiff')
 
-def getBlocos(img):
-    Blocos = []
-    for x in range(I.shape[0]/8):
-        for y in range(I.shape[1]/8):
-            Blocos.append(I[x*8:(x+1)*8, y*8:(y+1)*8])
-    return Blocos
 
-def blockshaped(arr, nrows, ncols):
+def blockShaped(arr, nrows, ncols):
     h, w = arr.shape
-    return (arr.reshape(h//nrows, nrows, -1, ncols)
-               .swapaxes(1,2)
-               .reshape(-1, nrows, ncols))
+    return (arr.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols))
 
-def getDct(bloc):
-    return dct(dct(bloc.T*1., norm='ortho').T , norm='ortho')
+def unblockShaped(arr, h, w):
+    #array, 512,512
+    n, nrows, ncols = arr.shape
+    return (arr.reshape(h//nrows, -1, nrows, ncols).swapaxes(1,2).reshape(h, w))
 
-def getIdct(dct):
-    return idct(idct(dct.T*1., norm='ortho').T , norm='ortho')
+def DCTransform(blocksIMG):
+    DCT = []
+    for block in blocksIMG:
+        DCT.append(dct(dct(block.T*1., norm='ortho').T , norm='ortho'))
+    return DCT
 
+def IDCTransform(blocksDCT):
+    IDCT = []
+    for block in blocksDCT:
+        IDCT.append(idct(idct(block.T*1., norm='ortho').T , norm='ortho').astype('int'))
+    return IDCT
 
-def snr_practical(s, eq):
-    """
-    s = signal
-    eq = erro quantificacao
-    ps = soma das amostras (de um sinal) ao quadrado / comprimento do sinal
-    pr = soma do erro de quantificacao ao quadrado / comprimento do erro de quant
-    """
-    ps = np.sum(s ** 2) / len(s)
-    pr = np.sum(eq ** 2) / len(eq)
-    return 10 * np.log10(ps/pr)
+def quantify(blocksDCT):
+    BQ = []
+    for block in blocksDCT:
+        BQ.append(np.round(block/(a*Q)).astype('int'))
+    return BQ
+
+def dequantify(blocksQ):
+    DCT = []
+    for block in blocksQ:
+        DCT.append(a*Q*block)
+    return DCT
 
 def twos_complement(bin):
     comp = ''
@@ -50,25 +54,90 @@ def twos_complement(bin):
             comp += '0'
     return comp
 
-def write(seqBits, fileName):
-    packed = np.packbits(seqBits)
-    np.save(fileName, packed)
-    return packed
+def code(blocksQ):
+    DC = []
+    code = []
 
+    for BQ in blocksQ:
+        # codificador DC
+        if len(DC) != 0:
+            DC_diff = BQ[0][0] - DC[-1]
+        else:
+            DC_diff = BQ[0][0]
+        # print "DC = %s" % DC_diff
+        DC.append(BQ[0][0])
+        DC_code = ''
+        if DC_diff < 0:
+            DC_bin = bin(DC_diff)[3:]
+            DC_code += K3.get(len(DC_bin)) + twos_complement(DC_bin)
+        elif DC_diff == 0:
+            DC_code += "00"
+        else:
+            DC_bin = bin(DC_diff)[2:]
+            DC_code += K3.get(len(DC_bin)) + DC_bin
 
-def read(fileName):
-    compressedFile = np.load(fileName)
-    seqBits = np.unpackbits(compressedFile)
-    return seqBits
+        code += map(int, DC_code)
 
-def unblockshaped(arr, h, w):
-    #array, 512,512
-    n, nrows, ncols = arr.shape
-    return (arr.reshape(h//nrows, -1, nrows, ncols)
-               .swapaxes(1,2)
-               .reshape(h, w))
+        # codificador AC
+        AC_code = ''
+        numZeros = 0
+        # print BQ
+        BQ_zz = BQ.flatten(order='F')[np.argsort(ind_zz)].astype(int)
+        # print BQ_zz
 
-def decode(seqBits, K3_inv, K5_inv):
+        # print "AC =",
+        i = 1
+        while i < len(BQ_zz):
+            if BQ_zz[i] != 0:
+                AC_code += K5.get((numZeros, len(bin(abs(BQ_zz[i]))[2:])))
+                # print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
+                if BQ_zz[i] < 0:
+                    AC_bin = bin(BQ_zz[i])[3:]
+                    AC_code += twos_complement(AC_bin)
+                    # print twos_complement(AC_bin),
+                elif BQ_zz[i] > 0:
+                    AC_code += bin(BQ_zz[i])[2:]
+                    # print bin(BQ_zz[i])[2:],
+                numZeros = 0
+                if i+1 >= len(BQ_zz):
+                    AC_code += K5.get((0,0))
+                    # print (0,0)
+                    break
+            else:
+                #se do i para a frente for tudo zero
+                if sum(BQ_zz[i:]!=0) == 0:
+                    AC_code += K5.get((0,0))
+                    # print (0,0)
+                    break
+                #se tivermos dentro do len e se nos proximos 15 forem todos zero
+                if (i + 15) < len(BQ_zz) and sum(BQ_zz[i:i+15]!=0) == 0:
+                    if BQ_zz[i+15] == 0:
+                        AC_code += K5.get((15,0))
+                        # print (15,0),
+                    else:
+                        AC_code += K5.get((15,len(bin(abs(BQ_zz[i+15]))[2:])))
+                        # print (15,len(bin(abs(BQ_zz[i+15]))[2:])),
+                        if BQ_zz[i+15] < 0:
+                            AC_bin = bin(BQ_zz[i+15])[3:]
+                            AC_code += twos_complement(AC_bin)
+                            # print twos_complement(AC_bin),
+                        elif BQ_zz[i+15] > 0:
+                            AC_code += bin(BQ_zz[i+15])[2:]
+                            # print BQ_zz[i+15],
+                    i += 16
+                    if i+16 >= len(BQ_zz):
+                        AC_code += K5.get((0,0))
+                        # print (0,0),
+                        break
+                    numZeros =0
+                    pass
+                numZeros +=1
+            i += 1
+
+        code += map(int, AC_code)
+    return code
+
+def decode(seqBits):
     blocos = []
     bloco = np.zeros(64)
     bloco_pos = 1
@@ -77,6 +146,7 @@ def decode(seqBits, K3_inv, K5_inv):
     # for i in range(len(seqBits)):
     i = 0
     DC = []
+
     while i < len(seqBits):
         symb += str(seqBits[i])
         if  isDC:
@@ -99,19 +169,24 @@ def decode(seqBits, K3_inv, K5_inv):
                 i += size
                 symb = ''
                 isDC = False
+                # print '\nAC =',
         else:
             if K5_inv.has_key(symb):
                 t = K5_inv.get(symb)
                 if t == (0,0):
+                    # print '(0, 0)',
                     bloco_pos = 1
                     blocos.append(np.reshape(bloco[ind_zz], (8,8)).T.astype(int))
+                    bloco = np.zeros(64)
                     isDC = True
                 elif t == (15, 0):
+                    # print '(15, 0)',
                     bloco_pos += 16
                 else:
                     num_zeros = t[0]
                     size = t[1]
                     num_bin = ''.join(map(str, seqBits[i+1:i+1+size]))
+                    # print '(' + str(num_zeros) +', '+ str(size) + ') ' + str(num_bin),
                     if num_bin[0] == '0':
                         num_bin = '-'+twos_complement(num_bin)
                     num = int(num_bin, 2)
@@ -120,152 +195,111 @@ def decode(seqBits, K3_inv, K5_inv):
                     i += size
                 symb = ''
         i += 1
-    #     if dic.has_key(symb):
-    #         data.append(dic.get(symb))
-    #         symb = ''
+    # print
     return blocos
+
+def SNR(original, quantificado):
+
+    original = np.asarray(original).reshape(-1).astype('float')
+    quantificado = np.asarray(quantificado).reshape(-1).astype('float')
+
+    erro = quantificado - original
+    SNR = 10.*np.log10(sum(original**2)/sum(erro**2))
+
+    return SNR
+
+
+def write(seqBits, fileName):
+    packed = np.packbits(seqBits)
+    np.save(fileName, packed)
+    return packed
+
+
+def read(fileName):
+    compressedFile = np.load(fileName)
+    seqBits = np.unpackbits(compressedFile)
+    return seqBits[:len(CODE)]
 
 
 if __name__ == "__main__":
-    # bloco_dct = dct(dct(bloco.T, norm='ortho').T , norm='ortho')
 
-    I = readImage('lena.tiff')
-    print "Image %s:\n%s\n" % (I.shape, I)
+    sizeIni = 1. * path.getsize('lena.tiff')
+    print "tamanho lena.tiff %s" % sizeIni
+    qualitys = [10,25,50,75,90]
+    snrs = []
+    txs  = []
 
-    Blocos = blockshaped(I,8,8)
-
-    #Blocos = getBlocos(I)
-
-
-    Blocos = np.asarray(Blocos)
-    # print Blocos.shape
-    # Blocos = unblockshaped(Blocos,512,512)
-    # print Blocos
+    for i in range(len(qualitys)):
 
 
-    qualidade = 50
-    a = quality_factor(qualidade)
-    print "alfa:\n%s\n" % a
+        quality = qualitys[i]
+        print 'comprimindo imagem com qualidade %s...' % quality
 
-    DC = []
-    code = []
+        I = readImage('lena.tiff')
+        # print "Image %s:\n%s\n" % (I.shape, I)
 
-    # compressao
-    for B in Blocos:
+        BLOCKS = blockShaped(I, I.shape[0]/64, I.shape[1]/64)
+        # print "Blocks:\n%s\n" % BLOCKS
 
-        print "--------------------------"
+        a = quality_factor(quality)
+        # print "alfa:\n%s\n" % a
 
-        # dct
-        C = getDct(B)
-        # print "DCT:\n%s\n" % C
+        DCT = DCTransform(BLOCKS)
+        # print "DCT:\n%s\n" % DCT
 
-        # idct
-        # B2 = idct(idct(C.T*1., norm='ortho').T , norm='ortho')
-        # print "IDCT (comparacao):\n%s\n%s\n" % (B, B2)
+        BQ = quantify(DCT)
+        # print "Quantified:\n%s\n" % BQ
 
-        # quantificacao
-        BQ = np.round(C/(a*Q)).astype('int')
-        print "Bloco quantificado:\n%s\n" % BQ
+        t0 = time()
+        CODE = code(BQ)
+        t1 = time()
+        print "time code: " + str(t1 - t0)
+        # print "code:\n%s\n" % CODE
 
-        # desquantificao
-        # C2 = a*Q*BQ
-        # print "Bloco desquantificado (comparacao):\n%s\n%s\n" % (C, C2)
+        write(CODE, 'lena'+str(quality))
+        CODE1 = read('lena'+str(quality)+'.npy')
+        # print "code new:\n%s\n" % CODE1
 
-        # codificador DC
-        if len(DC) != 0:
-            DC_diff = BQ[0][0] - DC[-1]
-        else:
-            DC_diff = BQ[0][0]
-        print "DC = %s" % DC_diff
-        DC.append(BQ[0][0])
-        DC_code = ''
-        if DC_diff < 0:
-            DC_bin = bin(DC_diff)[3:]
-            DC_code += K3.get(len(DC_bin)) + twos_complement(DC_bin)
-        elif DC_diff == 0:
-            DC_code += "00"
-        else:
-            DC_bin = bin(DC_diff)[2:]
-            DC_code += K3.get(len(DC_bin)) + DC_bin
+        t0 = time()
+        BQ1 = decode(CODE1)
+        t1 = time()
+        print "time decode: " + str(t1 - t0)
+        # print "Quantified new:\n%s\n" % BQ1
 
-        code += map(int, DC_code)
+        DCT1 = dequantify(BQ1)
+        # print "DCT new:\n%s\n" % DCT1
 
-        # codificador AC
-        AC_code = ''
-        numZeros = 0
-        # print BQ
-        BQ_zz = BQ.flatten(order='F')[np.argsort(ind_zz)].astype(int)
-        # print BQ_zz
+        BLOCKS1 = IDCTransform(DCT1)
+        # print "BLOCKS new:\n%s\n" % BLOCKS1
 
-        print "AC =",
-        # for i in range(1, len(BQ_zz)):
+        I1 = unblockShaped(np.asarray(BLOCKS1), 512, 512)
+        # print "Image new:\n%s\n" % I1
 
-        i = 1
-        while i < len(BQ_zz):
-            if BQ_zz[i] != 0:
-                print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
-                AC_code += K5.get((numZeros, len(bin(abs(BQ_zz[i]))[2:])))
-                # print (numZeros, len(bin(abs(BQ_zz[i]))[2:])),
-                # print "---0"
-                if BQ_zz[i] < 0:
-                    AC_bin = bin(BQ_zz[i])[3:]
-                    AC_code += twos_complement(AC_bin)
-                    print twos_complement(AC_bin),
-                    # print "---1"
-                elif BQ_zz[i] > 0:
-                    AC_code += bin(BQ_zz[i])[2:]
-                    print BQ_zz[i],
-                    # print "---2"
-                numZeros = 0
-            else:
-                #se do i para a frente for tudo zero
-                if sum(BQ_zz[i:]!=0) == 0:
-                    AC_code += K5.get((0,0))
-                    print (0,0)
-                    break
-                #se tivermos dentro do len e se nos proximos 15 forem todos zero
-                if (i + 15) < len(BQ_zz) and sum(BQ_zz[i:i+15]!=0) == 0:
-                    if BQ_zz[i+15] == 0:
-                        AC_code += K5.get((15,0))
-                        print (15,0),
-                    else:
-                        AC_code += K5.get((15,len(bin(abs(BQ_zz[i+15]))[2:])))
-                        print (15,len(bin(abs(BQ_zz[i+15]))[2:])),
-                    i += 16
-                    numZeros =0
-                    pass
-                numZeros +=1
-            i += 1
+        img = Image.fromarray(I1.astype('uint8'), 'L')
+        img.save("lenaJPEG"+str(quality)+".bmp")
 
-        code += map(int, AC_code)
+        snr = SNR(I,I1)
+        print "snr: %s" % snr
+        snrs.append(snr)
 
-        # print 'DC_code %s' % DC_code
-        # print 'AC_code %s' % AC_code
-        print "--------------------------\n"
+        sizeEnd = path.getsize('lena'+str(quality)+'.npy')
+        print "lena.npy size:  %s" % sizeEnd
 
-    print 'code %s' % code
-
-    write(code, 'lena')
-    code1 = read('lena.npy')
-
-    print 'code %s' % code1
-    Blocos_Q1 = decode(code1[:len(code1)-2], K3_inv, K5_inv)
-    Blocos_Q1_test = np.asarray(Blocos_Q1)
-    #shape errada ja aqui
-    print Blocos_Q1_test.shape
-
-    #print "Bloco quantificado:\n%s\n" % Blocos_Q1
-
-    Blocos1 = []
-
-    for Bloco_Q1 in Blocos_Q1:
-        C2 = a*Q*Bloco_Q1
-        B2 = idct(idct(C2.T*1., norm='ortho').T , norm='ortho')
-        Blocos1.append(B2)
-
-    Blocos1 = np.asarray(Blocos1)
-    print Blocos1.shape
-    #unblockshaped(Blocos1,512, 512)
+        tx = sizeIni/sizeEnd
+        print "taxa: %s" % tx
+        txs.append(tx)
+        print
 
 
-    # descompressao
+    plt.figure()
+    plt.grid(True)
+    plt.plot(snrs, txs)
+    plt.xlabel("SNR")
+    plt.ylabel("taxa de compressao")
+
+    x = Image.open("lena.tiff")
+    x.save("lena.jpeg", "JPEG", quality=50)
+    sizeJPG = path.getsize('lena.jpeg')
+    print "taxa jpg: %s" % (sizeIni/sizeJPG)
+
+    plt.show()
