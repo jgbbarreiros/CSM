d598218ab0a715bfa9b1ea2ddf1cf8179cd7b185
Joao Barreiros <jgbbarreiros@gmail.com>
2015-05-28 03:04:21 +0100

    metodo expand expande uma imagem

--- TP4/tp4.py
+++ TP4/tp4.py
@@ -11,6 +11,33 @@ def blockShaped(arr, nrows, ncols):
     h, w = arr.shape
     return (arr.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols))
 
+def expand(arr, px, corner='pixel'):
+    ex_arr = np.zeros((arr.shape[0]+2*px, arr.shape[1]+2*px))
+    ex_arr[px:h+px, px:w+px] = arr # inserir imagem i no meio
+    ex_arr[:px, px:px+w] = arr[0] # linas topo
+    ex_arr[px+h:, px:px+w] = arr[-1] # linhas de baixo
+    ex_arr[px:px+h,:px ] = arr.T[0][:,None] # linhas esquerda
+    ex_arr[px:px+h, px+w:] = arr.T[-1][:,None] # linhas direita
+    if corner == 'pixel':
+        ex_arr[:px, :px] = np.ones((px, px))*arr[0, 0] # top left corner
+        ex_arr[:px, px+w:] = np.ones((px, px))*arr[0, -1] # top right corner
+        ex_arr[px+h:, :px] = np.ones((px, px))*arr[-1, 0] # bottom left corner
+        ex_arr[px+h:, px+w:] = np.ones((px, px))*arr[-1, -1] # bottom right corner
+    elif corner == 'average':
+        ex_arr[:px, :px] = (arr[0, :px][::-1] + arr.T[0, :px][::-1][:,None]) / 2. # top left corner
+        ex_arr[:px, px+w:] = (arr[0, -px:][::-1] + arr.T[-1:, :px][:,None]) / 2. # top right corner
+        ex_arr[px+h:, :px] = (arr[-1, :px] + arr.T[0, -px:][::-1][:,None]) / 2. # bottom left corner
+        ex_arr[px+h:, px+w:] = (arr[-1, -px:][::-1] + arr.T[-1, -px:][::-1][:,None]) / 2. # bottom right corner
+    elif corner == 'inverted':
+        ex_arr[:px, :px] = np.fliplr(np.fliplr(arr[:px, :px]).T) # top left corner
+        ex_arr[:px, px+w:] = arr[:px, -px:].T # top right corner
+        ex_arr[px+h:, :px] = arr[-px:, :px].T # bottom left corner
+        ex_arr[px+h:, px+w:] = np.fliplr(np.fliplr(arr[-px:, -px:]).T) # bottom right corner
+    else:
+        # TODO raise exeption 'unknown corner value'
+        pass
+    return ex_arr
+
 
 if __name__ == "__main__":
 
@@ -18,28 +45,25 @@ if __name__ == "__main__":
     img1 = Image.open("bola_seq/bola_1.tiff")
     img1.save("bola_1.jpeg", "JPEG", quality=50)
     I = np.array(img1).astype('float')
-    I1 = mi.imread("bola_1.jpeg").astype('float')
+    arr = mi.imread("bola_1.jpeg").astype('float')
+    h = I.shape[0] # 240
+    w = I.shape[1] # 352
+    IS = expand(I, 15, corner='pixel')
+    plt.figure("Search image")
+    plt.imshow((IS), cmap='gray', interpolation='none')
 
     ## IMAGEM 2 - SUBTRACAO
-    P = np.array(Image.open("bola_seq/bola_2.tiff")).astype('float')
-    P -= I1 + 127.
-    img2 = Image.fromarray(P.astype('uint8'))
-    img2.save("bola_2.jpeg", "JPEG", quality=50)
-    P1 = mi.imread("bola_2.jpeg").astype('float')
-    P1 += I1 - 127.
-    img2 = Image.fromarray(P1.astype('uint8'))
-    img2.save("bola_2.bmp", "bmp")
+    # P = np.array(Image.open("bola_seq/bola_2.tiff")).astype('float')
+    # P -= I1 + 127.
+    # img2 = Image.fromarray(P.astype('uint8'))
+    # img2.save("bola_2.jpeg", "JPEG", quality=50)
+    # P1 = mi.imread("bola_2.jpeg").astype('float')
+    # P1 += I1 - 127.
+    # img2 = Image.fromarray(P1.astype('uint8'))
+    # img2.save("bola_2.bmp", "bmp")
 
     ## IMAGEM 2 - MOVIMENTO
-    BI1 = blockShaped(I1, 16, 16)
     P = np.array(Image.open("bola_seq/bola_2.tiff")).astype('float')
-    print P
-    PB = np.zeros((270, 382))
-    print PB
-    PB[16:240+16, 16:352+16] = P
-    plt.imshow((PB), cmap='gray', interpolation='none')
-
-    # for bi1 in BI1:
-    #     pass
+    PB = blockShaped(P, 16, 16)
 
     plt.show()
