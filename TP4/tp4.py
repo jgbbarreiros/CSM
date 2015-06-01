50f8b0b92f4aaa7ce642f874ff80a834cd1f5c6b
Joao Barreiros <jgbbarreiros@gmail.com>
2015-06-01 17:22:30 +0100

    Estrutura base do ex 3 (predição da frame)

--- TP4/tp4.py
+++ TP4/tp4.py
@@ -7,10 +7,6 @@ from os import path
 import scipy.misc as mi
 
 
-def blockShaped(arr, nrows, ncols):
-    h, w = arr.shape
-    return (arr.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols))
-
 def expand(arr, px, corner='pixel'):
     ex_arr = np.zeros((arr.shape[0]+2*px, arr.shape[1]+2*px))
     ex_arr[px:h+px, px:w+px] = arr # inserir imagem i no meio
@@ -38,49 +34,50 @@ def expand(arr, px, corner='pixel'):
         pass
     return ex_arr
 
+
 def mae(P, I):
-    return np.sum(P-I)
+    return np.sum(np.abs(P-I))
 
-def search(I, P_arr, type='block'):
 
-    if type == 'block':
-        block_size = 16
-        window_size = block_size + 15*2
-        h = I.shape[0]
-        w = I.shape[1]
-        hb = w/block_size
-        vb = h/block_size
-        P_coded_arr = []
-        P_coded_vec_arr = []
-        for P in P_arr:
-            P_coded = np.empty((h,w))
-            P_coded_vec = []
-            P_blocks = np.reshape(blockShaped(P, block_size, block_size), (16,16))
-            pos_num = [0, 0]
-            min_vec = [-15, -15]
-            min_mae = mae(P_blocks[pos[0], pos[1]] , I[pos[0]+x:pos[0]+x+block_size, pos[1]+y:pos[1]+y+block_size])
-            min_pos = [0,0]
-            for i in range(block_size**2):
-                pos = [pos_num[0]*block_size-15, pos_num[1]*block_size-15]
-                if (pos_num[0] >= hb-1):
-                    pos_num[0] = 1
-                    pos_num[1] += 1
-                else:
-                    pos_num[0] += 1
-                for y in range(window_size-16):
-                    for x in range(window_size-16):
-                        new_mae = mae(P_blocks[pos[0], pos[1]], I[pos[0]+x:pos[0]+x+block_size, pos[1]+y:pos[1]+y+block_size])
-                        if new_mae < min_mae:
-                            min_mae = new_mae
-                            min_vec = [x-15, y-15]
-                            min_pos = pos_num
-                P_coded[min_pos[0]+15:min_pos[1]+15] = min_mae
-                P_coded_vec.append(min_vec)
-            P_coded_arr.append(P_coded)
-            P_coded_vec_arr.append(P_coded_vec)
-        return [P_coded_arr, P_coded_vec_arr]
-    else:
-        return
+def getPImages():
+    # TODO vai buscar todas as images e devolve um array
+    return []
+
+
+def search(I, P, type='block'):
+    I_blocks = IToBlocks(I) # return blocos de 46x46 (janela de pesquisa)
+    P_blocks = PToBlocks(P) # return blocos de 16x16
+    P_coded_arr = [] # array de blocos P codificados
+    P_vec = [] # array de pares de vectores
+    for i in range(len(I_blocks)):
+        P_block_coded, P_block_vec = macroSearch(I_blocks[i], P_blocks[i])
+        P_coded_arr.append(P_block_coded)
+        P_vec.append(P_block_vec)
+    P_coded = unblock(P_coded_arr)
+    return [P_coded, P_vec]
+
+
+def IToBlocks(I):
+    # TODO blocos de 46x46 (janela de pesquisa)
+    return []
+
+
+def PToBlocks(I):
+    # TODO blocos de 16x16
+    return []
+
+
+def macroSearch(I_block, P_block):
+    # TODO pequisa de Blocos P em macro Bloco I e encontar a menor "mae"
+    return [] # bloco codificado e o respectivo par de vectores
+
+def blockShaped(arr, nrows, ncols):
+    h, w = arr.shape
+    return (arr.reshape(h//nrows, nrows, -1, ncols).swapaxes(1,2).reshape(-1, nrows, ncols))
+
+def unblock(P_coded_arr):
+    # TODO compor a imagem codificada a partir de uma lista de Blocos 16x16
+    return # bloco codificado
 
 
 if __name__ == "__main__":
@@ -94,9 +91,9 @@ if __name__ == "__main__":
     arr = mi.imread("bola_1.jpeg").astype('float')
     h = I.shape[0] # 240
     w = I.shape[1] # 352
-    IS = expand(I, 15, corner='pixel')
+    I_exp = expand(I, 15, corner='pixel')
     plt.figure("Search image")
-    plt.imshow((IS), cmap='gray', interpolation='none')
+    plt.imshow((I_exp), cmap='gray', interpolation='none')
 
     ## IMAGEM 2 - SUBTRACAO
     # P = np.array(Image.open("bola_seq/bola_2.tiff")).astype('float')
@@ -109,12 +106,15 @@ if __name__ == "__main__":
     # img2.save("bola_2.bmp", "bmp")
 
     ## IMAGEM 2 - MOVIMENTO
-    P = np.array(Image.open("bola_seq/bola_2.tiff")).astype('float')
-    P_arr = []
-    P_arr.append(P)
-    P_coded_arr, P_coded_vec_arr = search(IS, P_arr, type='block')
-    print P_coded_arr
-    print P_coded_vec_arr
+    P_coded_arr = []
+    P_vec_arr = []
 
+    P = np.array(Image.open("bola_seq/bola_2.tiff")).astype('float')
+    P_arr = getPImages()
+
+    for P in P_arr:
+        P_coded, P_vec = search(I_exp, P, type='block')
+        P_coded_arr.append(P_coded)
+        P_vec_arr.append(P_vec)
 
     plt.show()
