0cf3d810ed0faf1d3cb58b26e7d85f37f03c3a99
Joao Barreiros <jgbbarreiros@gmail.com>
2015-06-07 20:54:02 +0100

    Calculos para todos os ex do tp4 Taxa de compressao snr tempo de comp e descomp

--- TP4/tp4_ex3.py
+++ TP4/tp4_ex3.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+from os import path
+from time import time
+
 
 def getImages():
     return [Image.open("bola_seq/bola_"+str(i+1)+".tiff") for i in range(11)]
@@ -45,9 +48,8 @@ def saveDecompImages(comp_imgs):
 
 def showImages(comp_imgs):
     for i in range(len(comp_imgs)):
-        plt.figure("Image "+str(i+2))
+        plt.figure("Image "+str(i+1))
         plt.imshow((comp_imgs[i]), cmap='gray', interpolation='none')
-    plt.show()
 
 def expand(arr, px):
     h = arr.shape[0] # 240
@@ -113,19 +115,44 @@ def compose(I, P_vecs):
 def getBlock(block, vec):
     return block[vec[0]:vec[0]+16, vec[1]:vec[1]+16]
 
+def compRate():
+    size_init = sum(path.getsize("bola_seq/bola_"+str(i+1)+".tiff") for i in range(11))
+    size_comp = sum(path.getsize("ex3/comp/bola_"+str(i+1)+".jpeg") for i in range(11))
+    return float(size_init)/size_comp
+
+def snr(originals, finals):
+    snrs = []
+    for i in range(11):
+        original = np.asarray(originals[i]).reshape(-1).astype('float')
+        quantificado = np.asarray(finals[i]).reshape(-1).astype('float')
+        erro = quantificado - original
+        snrs.append(10.*np.log10(sum(original**2)/sum(erro**2)))
+    return snrs
+
 
 if __name__ == "__main__":
 
     imgs = getImages()
 
+    t0 = time()
     comp_imgs, vecs = compImages(imgs)
+    t1 = time()
 
     saveCompImages(comp_imgs)
 
     comp_imgs1 = getCompImages()
 
+    t2 = time()
     decomp_imgs = decompImages(comp_imgs1, vecs)
+    t3 = time()
 
     saveDecompImages(decomp_imgs)
 
     showImages(decomp_imgs)
+
+    print "Compression rate: %s" % compRate()
+    print "Compression time: %s" % (t1-t0)
+    print "Decompression time: %s" % (t3-t2)
+    print "Signal noise ratio: %s" % snr(imgs, decomp_imgs)
+
+    plt.show()
